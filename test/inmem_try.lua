28fdadbe985534ebb7161fab4b0adb2ce378e879
Masatoshi Teruya <mah0x211@gmail.com>
2016-02-02 14:54:03 +0900

    change: test/inmem_try.lua has been updated.

--- test/inmem_try.lua
+++ test/inmem_try.lua
@@ -3,25 +3,45 @@ local sleep = require('process').sleep;
 local InMem = require('cache.inmem');
 local mem = ifNil( InMem.new() );
 local key = 'key';
+local newkey = 'newkey';
 local val = {
     test = 1
 };
 local expires = 1;
 local v;
 
+-- invalid arguments
 ifNotNil( mem:get( key ) );
 ifTrue( mem:set() );
 ifTrue( mem:set( 1 ) );
+
+-- valid value
 ifNotTrue( mem:set( key, 1 ) );
+
+-- invalid ttl
 ifTrue( mem:set( key, val, 0/0 ) );
 
-ifNotTrue( mem:set( key, val, expires ) );
+-- copy val
+ifNotTrue( mem:set( key, val ) );
 v = ifNil( mem:get( key ) );
 ifEqual( tostring( v ), tostring( val ) );
 ifNotEqual( inspect( v ), inspect( val ) );
 
-ifNotTrue( mem:delete( key ) );
 
+-- invalid rename arguments
+ifTrue( mem:rename( key, 1 ) );
+
+-- rename
+v = ifNil( mem:get( key ) );
+ifNotTrue( mem:rename( key, newkey ) );
+ifNotNil( mem:get( key ) );
+ifEqual( tostring( v ), tostring( mem:get( newkey ) ) );
+
+
+-- delete
+ifNotTrue( mem:delete( newkey ) );
+
+-- test ttl
 ifNotTrue( mem:set( key, val, expires ) );
 sleep( expires );
 ifNotNil( mem:get( key ) );
